cbb6f4065f9b81adfe71e0ebe8718aac0adbec7b
rchekashov <romanrich89@gmail.com>
2020-11-08 23:54:50 +0300

    leetcode 28 35

---
+++ leetcode/AbsLeetCodeSolution.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public abstract class AbsLeetCodeSolution {
+    
+    public void executeTests() {
+        long startTime = System.nanoTime();
+        tests();
+        long endTime = System.nanoTime();
+
+        System.out.println(String.format("Took %d ns", (endTime - startTime)));
+        System.out.println(String.format("Used %.2f MB", (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024)));
+    }
+
+    public abstract void tests();
+
+    public void doAssert(boolean check) {
+        if (!check) throw new IllegalArgumentException("Fail!");
+    }
+}
---
+++ leetcode/LeetCode28_Implement_strStr.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+public class LeetCode28_Implement_strStr extends AbsLeetCodeSolution {
+
+    public static void main(String[] args) {
+        new LeetCode28_Implement_strStr().executeTests();
+    }
+
+    @Override
+    public void tests() {
+        doAssert(strStr("hello", "ll") == 2);
+        doAssert(strStr("aaaaa", "bba") == -1);
+        doAssert(strStr("aaa", "aaaaa") == -1);
+        strStr("mississippi", "issip");
+        strStr("mississippi", "issipi");
+        strStr("", "");
+    }
+
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        if (needle.length() > haystack.length()) return -1;
+        
+        int first = -1, i = 0, j = 0;
+        
+        while (i < haystack.length() && j < needle.length()) {
+            
+            if (needle.charAt(j) == haystack.charAt(i)) {
+                j++;
+                if (first == -1) first = i;
+            } else if (first != -1) {
+                j = 0;
+                i = first;
+                first = -1;
+            }
+            
+            i++;
+        }
+        
+        return j == needle.length() ? first : -1;
+    }
+
+}
