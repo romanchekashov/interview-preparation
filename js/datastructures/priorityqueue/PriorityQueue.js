20ec81abefc4d6a5122b37d67a63673a5c3a068a
romanchekashov <romanrich89@gmail.com>
2021-02-16 16:46:06 +0300

    reorginize file structure

---
+++ js/datastructures/priorityqueue/PriorityQueue.js
@@ -0,0 +1,17 @@
+/**
+ * A priority queue is an Abstract Data Type (ADT) offering methods that allow removal of the item with the
+ * maximum (or minimum) key value, insertion, and sometimes other operations.
+ *
+ * A Priority Queue(PQ) is an abstract data type(ADT) similar to a regular queue or stack data structure
+ * in which each element additionally has a "priority" associated with it.
+ * In a priority queue, an element with high priority is served before an element with low priority.
+ * In some implementations, if two elements have the same priority, they are served according to the order in which they were enqueued,
+ * while in other implementations, ordering of elements with the same priority is undefined.
+ *
+ * While priority queues are often implemented with heaps, they are conceptually distinct from heaps.
+ * A priority queue is a concept like "a list" or "a map"; just as a list can be implemented with a linked list or an array,
+ * a priority queue can be implemented with a heap or a variety of other methods such as an unordered array.
+ *
+ * IMPORTANT:
+ * Each node in a heap satisfies the heap condition, which states that every node’s key is larger(less) than (or equal to) the keys of its children.
+ */
