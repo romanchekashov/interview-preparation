54bb934268eaf30471ab9caa1edb19cc7ca43e45
rchekashov <romanrich89@gmail.com>
2023-01-28 15:42:01 +0100

    leetcode 1. Two Sum

--- js/leetcode/patterns/twopointers/TwoSum_leetcode1easy.js
+++ js/leetcode/patterns/twopointers/TwoSum_leetcode1easy.js
@@ -30,7 +30,7 @@ var twoSum = function(nums, target) {
 };
 
 /**
- * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ * Follow-up: Can you come up with an algorithm that is less than O(n^2) time complexity?
  *
  * Complexity analysis:
  * Time complexity : O(n).
@@ -53,9 +53,51 @@ var twoSum2 = function(nums, target) {
     return null;
 };
 
+/**
+ * Follow-up: Can you come up with an algorithm that is less than O(n^2) time complexity?
+ *
+ * Complexity analysis:
+ * Time complexity : O(n * log(n)).
+ * Space complexity : O(n).
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum2Pointers = function(nums, target) {
+    // O(n)
+    const numIndexMap = nums.reduce((acc, cur, curIndex) => {
+        if (acc.has(cur)) {
+            acc.set(cur, [acc.get(cur), curIndex]);
+        } else {
+            acc.set(cur, curIndex);
+        }
+        return acc;
+    }, new Map());
+
+    // Merge sort: O(n * log(n))
+    nums.sort((a, b) => a - b);
+
+    let start = 0, end = nums.length - 1;
+    while (start < end) {
+        const sum = nums[start] + nums[end];
+
+        if (sum === target) {
+            return Array.isArray(numIndexMap.get(nums[start])) ? numIndexMap.get(nums[start]) : [numIndexMap.get(nums[start]), numIndexMap.get(nums[end])];
+        } else if (sum > target) {
+            end--;
+        } else {
+            start++;
+        }
+    }
+
+    return null;
+};
+
 const solutions = [
     twoSum,
-    twoSum2
+    twoSum2,
+    twoSum2Pointers
 ];
 
 solutions.forEach((solution) => {
