36d5a80a3001c49f151423c89766a371786bea0c
rchekashov <romanrich89@gmail.com>
2023-02-05 19:53:35 +0100

    companies info

---
+++ js/companies/Palantir.js
@@ -0,0 +1,44 @@
+/**
+ * https://www.youtube.com/watch?v=8kSOHfP3-5I&list=PLqTLGbLI0CvlSIhivgMPicudJZO4s9xRQ
+ * https://palantir.events/palantirspringrecruitingevents
+ */
+/**
+ * Подался в Palantir в Лондоне на позицию Frontend Engineer.
+ * Был скрин с рекрутером, дальше virtual phone screen на 45 минут (15 минут разговор + задача medium).
+ * После этого прислали данные об virtual onsite и тут стало не совсем понятно, что будет на них.
+ * 1. REENGINEERING - есть какой-то код, в котором нужно исправить баги и расширить функционал
+ * 2. DECOMPOSITION - нужно разобрать какую-то комплексную проблему на мелкие части и решить эти их после разбивки. (Возможно это system design?)
+ * 3. WEB CODING - написать application на React, здесь все понятно.
+ * Кто-то что-то слышал о первых двух интервью? На каждый раунд дают 1 час.
+ */
+
+/**
+ * In learning interview they introduced a new custom concept for package installer such as npm,
+ * which can pull package from more than one repo, using multi threading.
+ *
+ * You have to code some functions understand classes from the python concurrent library.
+ * I was able to do 3/5 tasks in the 1 hr time (in which half hr was behavioral itself)
+ *
+ * During Decomposition interview, the question was to come up with a first cut solution
+ * for taxi drivers and suggest ideas to improve their life.
+ * You are given 8000 records of taxi data in London with 8 columns
+ * (id, fare, start location, end location, start time, end time, distance).
+ * You need to suggest idea, draw system components, come up with required apis
+ * and code if required, for a solution that can be developed and deployed in a week.
+ */
+
+/**
+ * For onsites I had 3 interviews: decomposition, systems design and reengineering.
+ *
+ * Decomposition - Given problem on high level come up with the plan
+ * for building the system solving this problem
+ * (e.g. Given client X has Z data already build system that can solve problem Y using the Z)
+ *
+ * Systems - Typical systems design interview ( it might involve some
+ * problem solving/algo discussion to related to the system you’re designing)
+ *
+ * Reengineering - Given code snippet from already existing code base
+ * you know that there is the bug, find the bug.
+ *
+ * This was interview experience for experienced engineer.
+ */
---
+++ js/companies/Pinterest.js
@@ -0,0 +1,9 @@
+/**
+ * Just interviewed with Pinterest.
+ * Solving a leetcode medium took me 45 min compared to typical 5-15min in a contest.
+ *
+ * I spent time writing tests, asking about the domain a bit to choose better names for types,
+ * functions and variables, explaining my design and data structure choices
+ * (pretty much every line), adding "const" whenever appropriate, using long meaningful names
+ * instead of i/j/n etc.
+ */
